c4d0af6a61a07be192cc4d229b4a93b3a785503f
Magno Felipe <mfts201116@gmail.com>
2023-08-18 16:01:21 -0300

    UserEntity - getters methods unit tests

---
+++ src/users/domain/entities/__tests__/unity/user.entity.spec.ts
@@ -0,0 +1,46 @@
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.firstName(),
+      email: faker.internet.email(),
+      senha: faker.internet.password(),
+    };
+    sut = new UserEntity(props);
+  });
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toBe(props.name);
+    expect(sut.props.email).toBe(props.email);
+    expect(sut.props.senha).toBe(props.senha);
+    expect(sut.props.created_at).toBeInstanceOf(Date);
+  });
+
+  it('Getter of name field', () => {
+    expect(sut.props.name).toBeDefined();
+    expect(sut.props.name).toEqual(props.name);
+    expect(typeof sut.props.name).toBe('string');
+  });
+
+  it('Getter of email field', () => {
+    expect(sut.props.email).toBeDefined();
+    expect(sut.props.email).toEqual(props.email);
+    expect(typeof sut.props.email).toBe('string');
+  });
+
+  it('Getter of senha field', () => {
+    expect(sut.props.senha).toBeDefined();
+    expect(sut.props.senha).toEqual(props.senha);
+    expect(typeof sut.props.senha).toBe('string');
+  });
+
+  it('Getter of created_at field', () => {
+    expect(sut.props.created_at).toBeDefined();
+    expect(sut.props.created_at).toBeInstanceOf(Date);
+  });
+});
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string;
+  email: string;
+  senha: string;
+  created_at?: Date;
+};
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get senha() {
+    return this.props.senha;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+}
