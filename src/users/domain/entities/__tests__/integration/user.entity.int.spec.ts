baa51dc9a41ad52b33ca0f44232cf77d0373646f
Magno Felipe <mfts201116@gmail.com>
2023-08-27 10:33:51 -0300

    Integration tests - validate email and password field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -14,7 +14,44 @@ describe('UserEntity integration tests', () => {
       props = { ...UserDataBuilder({}), name: '' };
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
 
+      props = { ...UserDataBuilder({}), name: 10 as any };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
       props = { ...UserDataBuilder({}), name: 'f'.repeat(256) };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+
+    it('Should throw an error when creating a user with invalid email', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        email: null,
+      };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), email: '' };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), email: 10 as any };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), email: 'f'.repeat(256) };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+    });
+
+    it('Should throw an error when creating a user with invalid password', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        password: null,
+      };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), password: '' };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), password: 10 as any };
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
+
+      props = { ...UserDataBuilder({}), password: 'f'.repeat(256) };
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError);
     });
   });
